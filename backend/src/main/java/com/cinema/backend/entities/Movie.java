8520665e880a1c3d97ebdc46fcdc9550329a4e29
literallytoast <131976518+bbp27735@users.noreply.github.com>
2024-09-21 17:05:47 -0400

    Updated Movie.java with required information
    Formatted Movie.java to have required elements for Front End
    Added annotations to prevent bad input from being added to the database
    
    Co-Authored-By: Gabe Livengood <gabe@ggliv.com>

--- backend/src/main/java/com/cinema/backend/entities/Movie.java
+++ backend/src/main/java/com/cinema/backend/entities/Movie.java
@@ -1,16 +1,62 @@
 package com.cinema.backend.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.URL;
+
+import java.util.List;
+
+
+
 
 @Entity
 public class Movie {
 
+  public enum AgeRating {
+    G,
+    PG,
+    PG13,
+    R,
+    NC17,
+  }
+
   @Id @GeneratedValue private long id;
 
   @NotEmpty public String title;
+
+  @NotNull public AgeRating ageRating;
+
+  @Min(0) @Max(5) @NotNull public Double reviewRating;
+
+  @NotEmpty @Column(name="movieCast") public List<@NotEmpty String> cast;
+
+  @NotEmpty public String director;
+
+  @NotEmpty public String synopsis;
+
+  @NotEmpty public List<@NotEmpty String> category;
+
+  @Min(0) @NotNull public Double childPrice;
+
+  @Min(0) @NotNull public Double adultPrice;
+
+  @Min(0) @NotNull public Double seniorPrice;
+
+  @Min(0) @NotNull public Double onlineFee;
+
+  @URL(regexp = "(http|https).*") public String posterUrl;
+
+  @URL(regexp = "https?:\\/\\/www.youtube.com\\/embed\\/[A-Za-z0-9_\\-]{11}") public String trailerUrl;
+
+  @NotNull public boolean isRunning;
+
+  //TODO Add list for screenshots/clips of movie later
 
   public long getId() {
     return this.id;
@@ -23,5 +69,18 @@ public class Movie {
    */
   public void copy(Movie other) {
     this.title = other.title;
+    this.ageRating = other.ageRating;
+    this.reviewRating = other.reviewRating;
+    this.cast = other.cast;
+    this.director = other.director;
+    this.synopsis = other.synopsis;
+    this.category = other.category;
+    this.childPrice = other.childPrice;
+    this.adultPrice = other.adultPrice;
+    this.seniorPrice = other.seniorPrice;
+    this.onlineFee = other.onlineFee;
+    this.posterUrl = other.posterUrl;
+    this.trailerUrl = other.trailerUrl;
+    this.isRunning = other.isRunning;
   }
 }
