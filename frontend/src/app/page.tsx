3aa4b74712e5f7371c1d8911959186fab678ffeb
Gabe Livengood <gabe@ggliv.com>
2024-09-25 10:49:54 -0400

    Make tailwind work on docker builds
    We weren't copying over the postcss config file on build, so things
    weren't loading.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import '../app/globals.css';
 import Navbar from "../app/components/Navbar";
 import SearchBar from "../app/components/SearchBar";
 import MovieCard from "../app/components/MovieCard";
