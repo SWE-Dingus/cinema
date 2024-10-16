d07ea2ac0d9c6121565520a45c99c94e41234bbe
Gabe Livengood <gabe@ggliv.com>
2024-10-16 11:10:34 -0400

    Add missing return

--- backend/src/main/java/com/cinema/backend/controllers/UsersController.java
+++ backend/src/main/java/com/cinema/backend/controllers/UsersController.java
@@ -1,48 +1,39 @@
 package com.cinema.backend.controllers;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import com.cinema.backend.entities.User;
 import com.cinema.backend.repositories.UserRepository;
 import jakarta.validation.Valid;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    @Autowired
-    public UsersController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+  @Autowired
+  public UsersController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
-    @PostMapping("/create")
-    public Map<String, String> createUser(@Valid @RequestBody User user) {
-        if (userRepository.findById(user.username) != null) {
-            //ToDo: Throw error need a different username
-        } else {
-            userRepository.save(user);
-            return new HashMap<>() {
-                {
-                    this.put("username", user.getUsername());
-            
-                }
-            };
+  @PostMapping("/create")
+  public Map<String, String> createUser(@Valid @RequestBody User user) {
+    if (userRepository.findById(user.username) != null) {
+      // ToDo: Throw error need a different username
+    } else {
+      userRepository.save(user);
+      return new HashMap<>() {
+        {
+          this.put("username", user.getUsername());
         }
+      };
     }
-    
-
+    return null;
+  }
 }
