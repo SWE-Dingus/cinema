0c2b1567a7906dff8108f2955d0734f8db64d183
literallytoast <131976518+bbp27735@users.noreply.github.com>
2024-10-21 17:18:10 -0400

    Update PaymentCardsController.java

--- backend/src/main/java/com/cinema/backend/controllers/PaymentCardsController.java
+++ backend/src/main/java/com/cinema/backend/controllers/PaymentCardsController.java
@@ -10,13 +10,7 @@ import com.cinema.backend.repositories.UserRepository;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -52,6 +46,9 @@ public class PaymentCardsController {
         .findById(id)
         .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
   }
+
+  @PutMapping("/edit")
+  public void setPaymentCard(@PathVariable long id) {}
 
   @GetMapping("/getAll")
   public List<PaymentCard> getAll() {
