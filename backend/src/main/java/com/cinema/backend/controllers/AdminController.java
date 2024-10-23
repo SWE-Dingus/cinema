9f8b96ddd48941b3402ebc312973f4a496364ab2
Gabe Livengood <gabe@ggliv.com>
2024-10-22 22:41:14 -0400

    Add endpoint for admin to get all users
    Auth TODO

--- backend/src/main/java/com/cinema/backend/controllers/AdminController.java
+++ backend/src/main/java/com/cinema/backend/controllers/AdminController.java
@@ -1,8 +1,26 @@
 package com.cinema.backend.controllers;
 
+import com.cinema.backend.entities.User;
+import com.cinema.backend.repositories.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
-public class AdminController {}
+public class AdminController {
+
+  private UserRepository userRepository;
+
+  @Autowired
+  public AdminController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @GetMapping("/getAllUsers")
+  public List<User> getAllUsers() {
+    return userRepository.findAll();
+  }
+}
