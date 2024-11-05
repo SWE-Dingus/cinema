20db22f9cab76ab0c60636f93fffd2f21add92fd
Gabe Livengood <gabe@ggliv.com>
2024-11-04 21:22:54 -0500

    Add functionality for emailing out promotions

--- backend/src/main/java/com/cinema/backend/controllers/PromotionsController.java
+++ backend/src/main/java/com/cinema/backend/controllers/PromotionsController.java
@@ -4,7 +4,10 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import com.cinema.backend.entities.Promotion;
 import com.cinema.backend.repositories.PromotionsRepository;
+import com.cinema.backend.repositories.UserRepository;
 import com.cinema.backend.services.AccountsService;
+import com.cinema.backend.services.EmailService;
+import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -23,17 +26,23 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/promotions")
 public class PromotionsController {
 
-  private final PromotionsRepository promotionsRepository;
   private final AccountsService accountsService;
+  private final EmailService emailService;
+  private final PromotionsRepository promotionsRepository;
+  private final UserRepository userRepository;
   private final HttpServletRequest request;
 
   @Autowired
   public PromotionsController(
-      PromotionsRepository promotionsRepository,
       AccountsService accountsService,
+      EmailService emailService,
+      PromotionsRepository promotionsRepository,
+      UserRepository userRepository,
       HttpServletRequest request) {
-    this.promotionsRepository = promotionsRepository;
+    this.emailService = emailService;
     this.accountsService = accountsService;
+    this.promotionsRepository = promotionsRepository;
+    this.userRepository = userRepository;
     this.request = request;
   }
 
@@ -50,7 +59,7 @@ public class PromotionsController {
   }
 
   @PutMapping("/update/{id}")
-  public void updatePaymentCard(@PathVariable long id, @Valid @RequestBody Promotion promotion) {
+  public void updatePaymentCard(@PathVariable String id, @Valid @RequestBody Promotion promotion) {
     accountsService.ensureAdmin(request);
     var dbPromotion =
         promotionsRepository.findById(id).orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
@@ -59,8 +68,37 @@ public class PromotionsController {
   }
 
   @DeleteMapping("/delete/{id}")
-  public void deletePaymentCard(@PathVariable long id) {
+  public void deletePaymentCard(@PathVariable String id) {
     accountsService.ensureAdmin(request);
     promotionsRepository.deleteById(id);
+  }
+
+  @PostMapping("/send/{id}")
+  public void sendPromotion(@PathVariable String id) {
+    accountsService.ensureAdmin(request);
+    var dbPromotion =
+        promotionsRepository
+            .findById(id)
+            .orElseThrow(
+                () ->
+                    new ResponseStatusException(
+                        NOT_FOUND, String.format("Promotion with ID %d not found", id)));
+    userRepository.findAll().stream()
+        .filter(u -> u.wantsMarketingEmails)
+        .forEach(
+            u -> {
+              try {
+                emailService.sendEmail(
+                    u.email,
+                    "Check out this new promotion from the cinema!",
+                    String.format(
+                        """
+            We have a new promotion for you! Enter the code %s at check-out for a %.2f%% off discount!""",
+                        dbPromotion.code, dbPromotion.discountPercent));
+              } catch (MessagingException e) {
+                System.err.println(
+                    "Failed to send promotion notification email to user " + u.email);
+              }
+            });
   }
 }
